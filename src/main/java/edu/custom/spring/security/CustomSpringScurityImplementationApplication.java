94b61e744d5d566c70d87fee469bdada8dce3ba1
cs99721 <razvan.parautiu@extern.consorsbank.de>
2021-08-19 13:58:06 +0300

    Added spring initialized project.

---
+++ src/main/java/edu/custom/spring/security/CustomSpringScurityImplementationApplication.java
@@ -0,0 +1,13 @@
+package edu.custom.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomSpringScurityImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomSpringScurityImplementationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/custom/spring/security/CustomSpringScurityImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.custom.spring.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomSpringScurityImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
