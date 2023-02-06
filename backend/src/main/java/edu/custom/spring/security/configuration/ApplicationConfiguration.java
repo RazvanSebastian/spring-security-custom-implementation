bd7a7d101005219a0243671725a982550f97ac72
Razvan Parautiu <r.parautiu-extern@leonhard-weiss.com>
2023-02-06 09:35:49 +0200

    Enable transactional management

--- backend/src/main/java/edu/custom/spring/security/configuration/ApplicationConfiguration.java
+++ backend/src/main/java/edu/custom/spring/security/configuration/ApplicationConfiguration.java
@@ -2,8 +2,10 @@ package edu.custom.spring.security.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
+@EnableTransactionManagement
 @Configuration
 public class ApplicationConfiguration {
 
